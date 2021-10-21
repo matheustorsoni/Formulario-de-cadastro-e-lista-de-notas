5e5d30d9934a760430d938ce292a3307ff97dead
Matheus Torsoni de Campos <91149036+matheustorsoni@users.noreply.github.com>
2021-10-21 13:42:10 -0300

    limpandop arquivos e adicioando area de texto app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+    <form>
+      <input type="text" placeholder="titulo"/>
+      <textarea placeholder="escreva sua nota..."/>
+      <button>Criar nota</button>
+    </form>
+  );
+  <ul>
+    <li>
+      <section>
+        <header>
+          <h3>Titulo</h3>
+        </header>
+        <p>Escreva sua nota</p>
+      </section>
+    </li>
+  </ul>
+  </section>
   );
 }
 
